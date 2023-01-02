c8b08b5ecb9c2bcc92739fb3407308dc19cd84f2
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-02 13:58:00 +0100

    rozwiazanie zadania

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        String product1Name = "Mleko";
+        String product1Producer = "Mlekowita";
+        double product1Price = 2.5;
+        String product2Name = "Czekolada";
+        String product2Producer = "Wedel";
+        double product2Price = 2.19;
+
+        System.out.println(product1Name + " " + product1Producer + " " + product1Price);
+        System.out.println(product2Name + " " + product2Producer + " " + product2Price);
+    }
+}
